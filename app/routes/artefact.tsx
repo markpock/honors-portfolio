ea41216963c6944920d8b261d45831dc8a637e15
markpock <markpock7@gmail.com>
2025-06-01 19:47:36 -0700

    Giving up for now

--- app/routes/artefact.tsx
+++ app/routes/artefact.tsx
@@ -2,6 +2,35 @@ import { NavLink, Outlet } from "react-router"
 
 export default function Artefact() {
   return <>
-    <Outlet/>
+    <div className="flex min-w-screen min-h-full flex-col items-center">
+      
+      <div className="items-center min-h-full max-w-3xl">
+        <div className="h-100 w-full relative">
+          <img className="absolute left-0 top-0 rounded-xl w-full h-full" src="../gavel.png" alt="A gavel."></img>
+          <div className="absolute left-0 top-0 z-1 rounded-xl
+          bg-gradient-to-b from-black/70 to-amber-50/10 h-100 w-full">
+          <div className="absolute left-0 bottom-0 p-10">
+            <p>Winter 2023 &mdash; Spring 2023. Committee member</p>
+            <p>Summer 2023 &mdash; Spring 2024. Chair</p>
+            <b className="font-serif text-3xl">STF Committee</b>
+          </div>
+          </div>
+        </div>
+      
+        <div className="h-full w-full p-5 pb-15">
+          <div className="h-full w-full p-5 bg-black/40 rounded-xl">
+            {/* <div className="font-serif text-xl">
+              Hello World
+            </div> */}
+            <Outlet/>
+          </div>
+        
+        </div>
+        
+          
+      </div>
+    </div>
+    
+    
   </>
 }
--- app/routes/artefact/stf.tsx
+++ app/routes/artefact/stf.tsx
@@ -1,3 +1,86 @@
 export default function () {
-    return <p>I am an artefact.</p>
+  return <div className="prose">
+    <p>
+      Oddly, the STF &mdash; that's the Student Technology Fee Committee
+        &mdash; was one of the first student government organizations
+        I ever found out about &mdash;
+        before ASUW, before GPSS, before SAF.
+      It was those charging stations, which continue to dot campus &mdash;
+        then the stickers on computers in seemingly every lab
+        from all disparate parts of campus.
+      When I joined the ASUW senate at the beginning of my sophomore year, a vacancy for
+        ASUW liaison to the STFC opened up &mdash; so I took it.
+      I spent the rest of that year as a committee member.
+      I gave up future opportunities as a teaching assistant for it &mdash;
+        committee meetings had regular time conflicts with course staff meetings,
+        and I opted to choose the former instead of the latter.
+    </p>
+
+    <p>
+      That spring, I stood for election as chair.
+      It was an odd election &mdash; nine voters, including myself,
+        and two candidates (again including myself),
+        one of whom (or perhaps both) didn't really want to run.
+      Yes, I agonized over whether it would be a good choice,
+        whether I ought instead to have focused on other pursuits.
+      I decided to run anyway.
+      In retrospect, it wasn't my finest choice.
+      I can't say I don't regret it, but I also can't say that I do.
+    </p>
+
+    <p>
+      My year as chair is something I feel like was deeply educational,
+        though for the life of me I could not tell you exactly what I learned.
+      Then again, the notion that learning must be codified to be real
+        poses itself a variety of problems, so maybe I did learn more
+        than I might imagine.
+      That year, the STF's hands &mdash; my hands &mdash; were bound
+        by the University of Washington's Financial Transformation,
+        which saw UW migrate from the old financial system it had used
+        to Workday.
+      In order to spend any university money, a budget needs to be
+        created.
+      On the old system, this was as easy as walking down the hall and
+        asking financial personnel to click a button.
+      On the new system, budgets can only be created by the central
+        authority, with whom a ticket needs to be filed, and in whose
+        hands the ability to spend money solely lies.
+      "When can we get worktags?" &mdash; this was the constant refrain.
+      As a result, not much money ended up getting spent.
+    </p>
+
+    <p>
+      To be sure, I had big hopes going into that position.
+      But as we were hampered from creating results,
+        team morale flagged, communication broke down &mdash;
+        the ship began to sink.
+      <i>My</i> morale flagged &mdash; my junior year was an intensely stressful
+        year, and STF was a significant portion of the reason why.
+      I could have chosen to stay on as chair &mdash; or, if I had had
+        the foresight, I might have angled to move somewhere else in
+        student government.
+      But after the stresses of that year, I chose not to.
+      I chose to step away.
+    </p>
+
+    <p>
+      During that year, I began to wonder if my aspirations towards leadership
+        were bogging me down.
+      I was always &mdash; perhaps still am &mdash; something of a contrarian;
+        who at the first was suspicious of working my way into the system,
+        of following the rat race.
+      When I began to feel that I was lagging behind others,
+        these suspicions turned into feverish stresses,
+        and into the desire to skip steps and leap up above through leadership,
+        whatever that might mean.
+      I am now much more suspicious of suspicion than I ever was.
+    </p>
+
+    <p>
+      The hermeneutical masters of suspicion showed that immanent to every
+        system is the conditions of its critique and collapse;
+        so too must this hold for the system of critique.
+      What a torture; to see clearly (read: "to see clearly is a torture" or "oh, what torture -- if only I had seen clearly").
+    </p>
+  </div>
 }
